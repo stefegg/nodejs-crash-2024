7aed3e74bd0e3a13212235bd672749bba5b0269c
stefegg <stephenegbert@gmail.com>
2024-08-10 17:47:25 -0400

    init / http module

---
+++ index.js
@@ -0,0 +1,9 @@
+// const { generateRandomNumber, celciusToFahrenheit } = require("./utils");
+
+// console.log(generateRandomNumber());
+
+// console.log(celciusToFahrenheit(0));
+
+import { getPosts, getPostsLength } from "./postController.js";
+
+console.log(getPosts(), getPostsLength());
