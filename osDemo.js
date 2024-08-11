93ef1956fa4ac176954e9a7c2b152582cf1830a1
stefegg <stephenegbert@gmail.com>
2024-08-10 21:35:23 -0400

    os module

---
+++ osDemo.js
@@ -0,0 +1,19 @@
+import os from "os";
+
+// gives info about your system
+
+//userInfo()
+
+console.log(os.userInfo());
+
+//totalmem()
+
+console.log(os.totalmem());
+
+//freemem()
+
+console.log(os.freemem());
+
+//cpus()
+
+console.log(os.cpus());
